803efbaecc2471ebe15d361647be15b400d736c9
Nikita Abramov <n8ron@Nikitas-MacBook-Pro.local>
2021-05-07 18:00:06 +0300

    add type handler logic

--- src/main/java/ru/hse/plugin/TypedHandler.java
+++ src/main/java/ru/hse/plugin/TypedHandler.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Locale;
 
 public class TypedHandler extends TypedHandlerDelegate {
@@ -17,15 +18,10 @@ public class TypedHandler extends TypedHandlerDelegate {
     @NotNull
     @Override
     public Result charTyped(char c, @NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
-//        StorageData storageData = ServiceManager.getService(StorageData.class);
-//        synchronized (StorageData.class) {
-//            StorageData.getInstance().inc();
-//        }
-//        Messages.showInfoMessage("Nice cock!!!!!!!!!", "ATTENTiON");
-//        storageData.inc();
-//        if (doc.getText().toLowerCase().contains("cock")) {
-//            Messages.showInfoMessage("Nice cock!!!!!!!!!", "ATTENTiON");
-//        }
+        List<Metric> metrics = StorageData.getInstance().metrics;
+        for (Metric metric : metrics) {
+            metric.update(project, editor, file);
+        }
         return Result.STOP;
     }
 
