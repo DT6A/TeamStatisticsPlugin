6b872ac4222fcad95bf389b784dca1983fbfe477
Nikita Abramov <n8ron@Nikitas-MacBook-Pro.local>
2021-05-07 18:34:12 +0300

    fix bug with incorrect counting of word, in which we put letter in middle

--- src/main/java/ru/hse/plugin/WordCounter.java
+++ src/main/java/ru/hse/plugin/WordCounter.java
@@ -12,20 +12,29 @@ import org.jetbrains.annotations.NotNull;
 public class WordCounter implements Metric {
 
     private final String word;
+    private final int length;
     private int numberOfOccurrences;
 
     public WordCounter(String word) {
         this.word = word;
+        length = word.length();
     }
 
     @Override
     public boolean update(@NotNull Project project, @NotNull Editor editor, @NotNull PsiFile file) {
         int offset = editor.getCaretModel().getOffset();
         Document document = editor.getDocument();
-        String substring = document.getText(new TextRange(offset - word.length(), offset));
-        if (word.equals(substring)) {
-            numberOfOccurrences++;
-            return true;
+        // TODO +2 или +1....
+        String substring = document.getText(new TextRange(offset - length - 2,
+                offset + length + 2));
+        for (int start = 0; start < substring.length() - length - 1; start++) {
+            if (substring.charAt(start) == ' ' &&
+                    substring.charAt(start + length + 1) == ' ') {
+                if (substring.substring(start + 1, start + length).equals(word)) {
+                    numberOfOccurrences++;
+                    return true;
+                }
+            }
         }
         return false;
     }
