ff9abba241f829b27393ea7398782b47de385262
Lebedev Egor <egorikl@list.ru>
2021-06-02 19:42:27 +0300

    Add metric wrapper

---
+++ src/main/java/ru/hse/plugin/metrics/commons/util/MetricWrapper.java
@@ -0,0 +1,23 @@
+package ru.hse.plugin.metrics.commons.util;
+
+import org.jetbrains.annotations.NotNull;
+import ru.hse.plugin.metrics.abstracts.Metric;
+
+public class MetricWrapper {
+    @NotNull
+    private final Metric metric;
+
+    public MetricWrapper(@NotNull Metric metric) {
+        this.metric = metric;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Metric && metric.isSame((Metric) o);
+    }
+
+    @Override
+    public int hashCode() {
+        return metric.hashSame();
+    }
+}
