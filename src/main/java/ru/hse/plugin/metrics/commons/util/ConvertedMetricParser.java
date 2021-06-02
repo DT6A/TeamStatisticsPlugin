0be0729c74f76069bcd2a3b349ecc3ce274600d1
Lebedev Egor <egorikl@list.ru>
2021-06-02 21:20:30 +0300

    Refactor

--- src/main/java/ru/hse/plugin/metrics/commons/util/ConvertedMetricParser.java
+++ src/main/java/ru/hse/plugin/metrics/commons/util/ConvertedMetricParser.java
@@ -28,44 +28,39 @@ public final class ConvertedMetricParser {
 
         switch (parts[0]) {
             case WORD_COUNTER:
-                if (parts.length != 3) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(3, parts, metric);
                 return new WordCounter(parts[1], Integer.parseInt(parts[2]));
             case CHAR_COUNTER:
-                if (parts.length != 3) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(3, parts, metric);
                 return new CharCounter((char) Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
             case ALL_CHAR_COUNTER:
-                if (parts.length != 10 + ('z' - 'a' + 1) + 1) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(10 + ('z' - 'a' + 1) + 1, parts, metric);
                 return new AllCharCounter(
-                        Arrays.stream(parts).skip(1).map(Integer::parseInt).collect(Collectors.toList())
+                        Arrays.stream(parts)
+                                .skip(1)
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList())
                 );
             case MAX_OPENED_PROJECTS:
-                if (parts.length != 2) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(2, parts, metric);
                 return new MaxOpenedProjects(Integer.parseInt(parts[1]));
             case PROJECT_OPENS_NUMBER:
-                if (parts.length != 2) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(2, parts, metric);
                 return new ProjectOpensNumber(Integer.parseInt(parts[1]));
             case COMMIT_COUNTER:
-                if (parts.length != 2) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(2, parts, metric);
                 return new CommitCounter(Integer.parseInt(parts[1]));
             case SPECIFIC_BRANCH_COMMIT_COUNTER:
-                if (parts.length != 3) {
-                    throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
-                }
+                validateLength(3, parts, metric);
                 return new SpecificBranchCommitCounter(Integer.parseInt(parts[1]), parts[2]);
             default:
                 throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
+        }
+    }
+
+    public static void validateLength(int length, String[] parts, String metric) {
+        if (parts.length != length) {
+            throw new RuntimeException("Parse error, could not parse \"" + metric + "\"");
         }
     }
 }
